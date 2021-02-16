1f50bfd4fa0c9d2f1597ddfb2196a91dcf221db8
t4t5u0 <ymmtryk0902@gmail.com>
2021-02-16 10:14:40 +0900

    [add] とりあえずダイスを振れるようになった

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use rand::prelude::thread_rng;
+use rand::prelude::Rng;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let (n, _, m) = arg_parse(&args[1]);
+    let x = roll(n, m);
+    let x_sum: u128 = x.iter().sum();
+    println!("{}d{} is : {:?} = {}", n, m, x, x_sum);
+}
+
+fn roll(n: usize, m: u128) -> Vec<u128> {
+    let mut rng = thread_rng();
+    let distr = rand::distributions::Uniform::new(1, m + 1);
+    let mut nums = vec![0u128; n];
+    for x in &mut nums {
+        *x = rng.sample(distr);
+    }
+    return nums;
+}
+
+fn arg_parse(arg: &String) -> (usize, String, u128) {
+    let result: Vec<&str> = arg.split("d").collect();
+    let n: usize = result[0].parse().unwrap();
+    let m: u128 = result[1].parse().unwrap();
+    return (n, "d".to_string(), m);
+}
+
+// fn index(arg: String) -> i32 {
+
+// }
+
+// fn roll(n: i32, m: i32) -> i32 {
+
+// }
